de4ade77e5a5a3d15aa3be935df99a71074a1133
Bor1sStan <s9110039682@yandex.ru>
2022-03-07 23:41:39 +0300

    feat/popup: доделать копку лайка

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+
+// Пишем скрипт для активации кнопки редактирования профиля
+// Добавляем модификатор открытия попапа
+
 const editButton = document.querySelector(".profile__edit-button");
 
 editButton.addEventListener("click", function() {
@@ -9,7 +13,8 @@ function showPopup() {
    popup.classList.add("popup_opened");
 }
 
-
+// Клик крестика для закрытия попапа
+// Удаляем модификатор открытия
 
 const closeButton = document.querySelector(".popup__close-button");
 
@@ -20,4 +25,33 @@ closeButton.addEventListener("click", function() {
 function closePopup() {
    const popup = document.querySelector(".popup");
    popup.classList.remove("popup_opened");
+}
+
+
+
+// Пишем скрипт для активациии кнопки сердечка в елементах
+// Добавляем модификатор для темного сердечка
+
+const ableLikeButton = document.querySelectorAll(".elements__like-button")
+
+ableLikeButton.addEventListener('click', function () {
+   showBlackButton()
+})
+
+function showBlackButton() {
+   const blackButton = document.querySelectorAll('.elements__like-button')
+   blackButton.classList.add('elements__like-button_active')
+}
+
+// Удаляем модификатор для белого сердечка
+
+const disableLikeButton = document.querySelector("elements__like-button_active")
+
+disableLikeButton.addEventListener('click', function () {
+   hideBlackButton()
+})
+
+function hideBlackButton() {
+   const whiteButton = document.querySelector('elements__like-button_active')
+   whiteButton.classList.remove('elements__like-button_active')
 }
