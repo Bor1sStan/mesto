0b5c2ecc73dc9bd6f32d644daf3f355470c99a34
Bor1sStan <s9110039682@yandex.ru>
2022-03-09 12:46:50 +0300

    feat/jscript: правлю скрипт

--- scripts/script.js
+++ scripts/script.js
@@ -27,16 +27,57 @@ function closePopup() {
    popup.classList.remove("popup_opened");
 }
 
+// Пишем скрипт для активации лайка
+// Добавляем модификатор темного сердечка
+
+const elements = document.querySelector('.elements');
+const likeButton = elements.querySelector('.elements__like-button');
+likeButton.addEventListener('click', function () {
+   blackLike()
+})
+
+function blackLike() {
+   const like = document.querySelector('.elements__like-button');
+   like.classList.add('elements__like-button_active');
+}
+
+// Дописать скипрт для деактивации лайка
+// Убрать модификатор для белого сердечка
+
+const dislikeButton = document.querySelector('.elements__like-button_active');
+dislikeButton.addEventListener('click', function() {
+   whiteLike()
+})
+
+function whiteLike () {
+   const dislike = document.querySelector('.elements__like-button_active')
+   dislike.classList.remove('elements__like-button_active')
+}
+ 
 
 
+// Написать скрипт для редактирования имени и информации о себе
 
-// Учебный тренажер яндекса
-/*
-const myName = prompt('Как Вас зовут?', '');
-console.log('Здравствуйте, ' + myName.slice(0, 1).toUpperCase() + myName.slice(1));
-*/
+// Находим форму в DOM
+const formElement = document.querySelector('.user-data-form')
+// Находим поля формы в DOM
+const nameInput = document.querySelector('.user-data-form__name')
+const jobInput = document.querySelector('.user-data-form__job')
 
-let blokPoem = 'Ночь. Улица. Фонарь. Аптека';
-let blokArray = blokPoem.split('.');
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
 
-console.log(blokArray); // ["Ночь", "Улица", "Фонарь", "Аптека"]
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
