e25857f5110f95d69be2fbdc0f8af52728514a23
Bor1sStan <s9110039682@yandex.ru>
2022-03-09 21:25:30 +0300

    добавлена обработка данных

--- scripts/script.js
+++ scripts/script.js
@@ -31,53 +31,40 @@ function closePopup() {
 // Добавляем модификатор темного сердечка
 
 const elements = document.querySelector('.elements');
-const likeButton = elements.querySelector('.elements__like-button');
-likeButton.addEventListener('click', function () {
-   blackLike()
+elements.addEventListener('click', function(event) {
+   if (event.target.className.includes("elements__like-button")) {
+      toggleLike(event.target)
+   }    
 })
 
-function blackLike() {
-   const like = document.querySelector('.elements__like-button');
-   like.classList.add('elements__like-button_active');
+function toggleLike(like) {
+   like.classList.toggle('elements__like-button_active');
 }
-
-// Дописать скипрт для деактивации лайка
-// Убрать модификатор для белого сердечка
-
-const dislikeButton = document.querySelector('.elements__like-button_active');
-dislikeButton.addEventListener('click', function() {
-   whiteLike()
-})
-
-function whiteLike () {
-   const dislike = document.querySelector('.elements__like-button_active')
-   dislike.classList.remove('elements__like-button_active')
-}
- 
 
 
 // Написать скрипт для редактирования имени и информации о себе
 
-// Находим форму в DOM
-const formElement = document.querySelector('.user-data-form')
-// Находим поля формы в DOM
-const nameInput = document.querySelector('.user-data-form__name')
-const jobInput = document.querySelector('.user-data-form__job')
-
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+function formSubmitHandler (event) {
+    event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
 
     // Получите значение полей jobInput и nameInput из свойства value
+const profileName = document.querySelector('.profile__name')
+const profileJob = document.querySelector('.profile__job')
+const nameInput = document.querySelector('.user-data-form__name')
+const jobInput = document.querySelector('.user-data-form__job')
+
+profileName.textContent = nameInput.value;
+profileJob.textContent = jobInput.value;
 
     // Выберите элементы, куда должны быть вставлены значения полей
-
+closePopup();
     // Вставьте новые значения с помощью textContent
 }
-
+const formElement = document.querySelector('.user-data-form')
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler); 
