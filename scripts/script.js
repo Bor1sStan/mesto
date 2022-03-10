e2ec3bffd6506e0ff3526b4cf3cffdf089987afe
Bor1sStan <s9110039682@yandex.ru>
2022-03-10 10:53:34 +0300

    Первая проба

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,7 @@ function showPopup() {
 }
 
 // Клик крестика для закрытия попапа
-// Удаляем модификатор открытия
+// Удаляем модификатор открытия попапа
 
 const closeButton = document.querySelector(".popup__close-button");
 
@@ -27,8 +27,7 @@ function closePopup() {
    popup.classList.remove("popup_opened");
 }
 
-// Пишем скрипт для активации лайка
-// Добавляем модификатор темного сердечка
+// Пишем скрипт для активации и деактивации лайка карточки
 
 const elements = document.querySelector('.elements');
 elements.addEventListener('click', function(event) {
@@ -41,17 +40,10 @@ function toggleLike(like) {
    like.classList.toggle('elements__like-button_active');
 }
 
+// Пишем скрипт для ввода данных в поля попапа. Закрытие попапа при нажатии кнопки Сохранить. 
 
-// Написать скрипт для редактирования имени и информации о себе
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (event) {
-    event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
+    event.preventDefault(); 
 const profileName = document.querySelector('.profile__name')
 const profileJob = document.querySelector('.profile__job')
 const nameInput = document.querySelector('.user-data-form__name')
@@ -60,11 +52,9 @@ const jobInput = document.querySelector('.user-data-form__job')
 profileName.textContent = nameInput.value;
 profileJob.textContent = jobInput.value;
 
-    // Выберите элементы, куда должны быть вставлены значения полей
 closePopup();
-    // Вставьте новые значения с помощью textContent
 }
+
 const formElement = document.querySelector('.user-data-form')
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+
 formElement.addEventListener('submit', formSubmitHandler); 
