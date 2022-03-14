9bbb3f3fd9a7e04f9af54cf64af64014fb7bcb7f
Bor1sStan <s9110039682@yandex.ru>
2022-03-14 20:03:19 +0300

    делаем js

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,58 @@
 
-// Пишем скрипт для активации кнопки редактирования профиля
-// Добавляем модификатор открытия попапа
+// Прописываем все константы
 
-const editButton = document.querySelector(".profile__edit-button");
+const editButton = document.querySelector(".profile__edit-button"); // переменная кнопки редактирования
+const closeButton = document.querySelector(".popup__close-button"); // переменная кнопки закрытия
+const elements = document.querySelector('.elements'); // переменная перебора элементов карточек
+const profileName = document.querySelector('.profile__name'); // переменная имени профиля
+const profileJob = document.querySelector('.profile__job'); // переменная работы профиля 
+const nameInput = document.querySelector('#name'); // переменная импута имени профиля
+const jobInput = document.querySelector('#job'); // переменная импута работы профиля
+const formElement = document.querySelector('.user-data-form'); // переменная forElement
+const popup = document.querySelector(".popup"); // переменная popup
 
-editButton.addEventListener("click", function() {
-   showPopup()
-})
 
+//  Прописываем все функции
+
+// функция открытия попапа showPopup
 function showPopup() {
-   const popup = document.querySelector(".popup");
    popup.classList.add("popup_opened");
 }
 
-// Клик крестика для закрытия попапа
-// Удаляем модификатор открытия попапа
-
-const closeButton = document.querySelector(".popup__close-button");
-
-closeButton.addEventListener("click", function() {
-   closePopup()
-})
-
+// функция закрытия попапа closePopup
 function closePopup() {
-   const popup = document.querySelector(".popup");
    popup.classList.remove("popup_opened");
 }
 
-// Пишем скрипт для активации и деактивации лайка карточки
 
-const elements = document.querySelector('.elements');
+ 
+//  Прописываем все слушатели событий 
+
+// клик открытия попапа
+editButton.addEventListener("click", showPopup);
+
+// клик закрытия попапа
+closeButton.addEventListener("click",  closePopup);
+
+// клик для лайка карточки
 elements.addEventListener('click', function(event) {
    if (event.target.className.includes("elements__like-button")) {
       toggleLike(event.target)
    }    
 })
 
+// отправка данных в форму профиля 
+formElement.addEventListener('submit', formSubmitHandler);
+
 function toggleLike(like) {
    like.classList.toggle('elements__like-button_active');
 }
 
-// Пишем скрипт для ввода данных в поля попапа. Закрытие попапа при нажатии кнопки Сохранить. 
+// пишем скрипт для ввода данных в поля попапа. Закрытие попапа при нажатии кнопки Сохранить. 
 
 function formSubmitHandler (event) {
     event.preventDefault(); 
-const profileName = document.querySelector('.profile__name')
-const profileJob = document.querySelector('.profile__job')
-const nameInput = document.querySelector('#name')
-const jobInput = document.querySelector('#job')
-
 profileName.textContent = nameInput.value;
 profileJob.textContent = jobInput.value;
-
 closePopup();
 }
-
-const formElement = document.querySelector('.user-data-form')
-
-formElement.addEventListener('submit', formSubmitHandler); 
