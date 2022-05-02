a6cf91dafaaa3c2ad28eb71633c7a47b6d9a5710
Bor1sStan <s9110039682@yandex.ru>
2022-05-02 15:22:19 +0300

    try alt object fit

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -75,7 +75,7 @@ export class FormValidator {
   resetValidation() {
     // this.toggleButtonState();
       this._inputList.forEach((inputElement) => {
-      this._hideInputError(inputElement);
+        this._hideInputError(inputElement);
     });
   }
 
